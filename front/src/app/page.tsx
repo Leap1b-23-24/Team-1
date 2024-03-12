1f22942de6726ed0db4fc99958a1604c2a18e1dc
Батмандал Бямба <23LP5716@G301-CC0203-3.local>
2024-03-12 14:30:48 +0800

    product header done

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,13 +1,13 @@
-"use client"
+"use client";
+import { ProductHeader } from "@/components/ProductComponents";
 import { ProductFilter } from "@/components/ProductComponents/ProductFilter";
 import { Stack } from "@mui/material";
 import { useState } from "react";
 
 export default function Home() {
-
   return (
-<>
-
-</>
+    <>
+      <ProductHeader />
+    </>
   );
 }
--- front/src/components/ProductComponents/index.ts
+++ front/src/components/ProductComponents/index.ts
@@ -3,3 +3,4 @@ export * from "./ProductClothesRowHeader";
 export * from "./ProductSide-Bar";
 export * from "./AddProductButton";
 export * from "./ProductFilter";
+export * from "./ProductHeader";
