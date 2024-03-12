cb8389439497cd08694cf53e9888ffb8694188c8
alwayswannaflyyy <bezsama0817@gmail.com>
2024-03-12 09:39:33 +0800

    addbutton

---
+++ front/src/components/AddProductButton.tsx
@@ -0,0 +1,15 @@
+import { Stack, Typography } from "@mui/material";
+
+export const AddProductButton = () => {
+  <Stack
+    color={"#121316"}
+    width={"280px"}
+    height={"48px"}
+    borderRadius={"8px"}
+    justifyContent={"center"}
+    alignItems={"center"}
+  >
+    <Typography color={"#FFFFFF"}>+</Typography>
+    <Typography color={"#FFFFFF"}>Бүтээгдэхүүн нэмэх</Typography>
+  </Stack>;
+};
