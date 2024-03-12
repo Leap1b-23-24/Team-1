174108067009c85bff30b15286fb043f11c3128d
Tsolmon Togtokhsaikhan <23LP9801@G301-CC0188.local>
2024-03-12 11:01:53 +0800

    hyanaarai hu

---
+++ front/src/components/ProductComponents/ProductFilter.tsx
@@ -0,0 +1,47 @@
+"use client"
+import { Stack, TextField, Typography } from "@mui/material";
+import Image from "next/image";
+import { ProductFilterDropdownCard } from "./ProductFilterDropdownCard";
+import { Dispatch, SetStateAction } from "react";
+import { Search } from "@mui/icons-material";
+
+const mapCate=[
+    "women",
+    "men",
+    "children"
+]
+
+const mapPrice=[
+    "lowest to ",
+    "highest to "
+]
+
+type ProductFilterProps={
+    setCategory:Dispatch<SetStateAction<string>>;
+    setPrice: Dispatch<SetStateAction<string>>;
+    setDate:Dispatch<SetStateAction<string>>
+    setSearch:Dispatch<SetStateAction<string>>
+}
+
+export const ProductFilter = (props:ProductFilterProps) => {
+    const {setCategory, setPrice, setDate, setSearch} = props;
+
+  return (
+    <Stack direction="row" width="100%" justifyContent="space-between" >
+      <Stack direction="row" gap={1.5}>
+       <ProductFilterDropdownCard svg={"/category.svg"} title="Ангилал" mapArr={mapCate} setState={setCategory}/>
+       <ProductFilterDropdownCard svg={"/attach_money.svg"} title="Үнэ" mapArr={mapPrice} setState={setPrice}/>
+       <ProductFilterDropdownCard svg={"/calendar_today.svg"} title="Сараар" setState={setDate}/>
+      </Stack>
+      <Stack direction={"row"} position={"relative"}>
+        <Stack position={"absolute"} left={8} top={8}>
+         <Search/>
+        </Stack>
+            <TextField type="text" placeholder="Бүтээгдэхүүний нэр, SKU, UPC"  InputProps={{style:{width:"419px", height:"40px", paddingLeft:"32px"}}}
+            onChange={(event)=>{
+                setSearch(event?.target.value);
+            }}/>
+      </Stack>
+    </Stack>
+  );
+};
---
+++ front/src/components/ProductComponents/ProductFilterDropdownCard.tsx
@@ -0,0 +1,73 @@
+"use state";
+import {
+  KeyboardArrowDownOutlined,
+  KeyboardArrowUpOutlined,
+} from "@mui/icons-material";
+import { Input, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import { Dispatch, SetStateAction, useState } from "react";
+
+type ProductFilterDropdownCardProps = {
+  svg: string;
+  title: string;
+  mapArr?: string[];
+  setState: Dispatch<SetStateAction<string>>;
+};
+
+export const ProductFilterDropdownCard = (
+  props: ProductFilterDropdownCardProps
+) => {
+  const { svg, title, mapArr, setState } = props;
+  const [isShown, setIsShown] = useState(false);
+
+  return (
+    <Stack>
+      <Stack
+        direction="row"
+        gap={1}
+        borderRadius={2}
+        p="8px 12px"
+        border="1px solid #ECEDF0"
+        alignItems={"center"}
+        onClick={() => {
+          setIsShown((prev) => !prev);
+        }}
+      >
+        <Image src={svg} alt="svg" width={24} height={24} />
+        <Typography fontWeight={600} fontSize={14} color={"#3F4145"}>
+          {title}
+        </Typography>
+        {isShown ? <KeyboardArrowUpOutlined /> : <KeyboardArrowDownOutlined />}
+      </Stack>
+      {title !== "Сараар" ? (
+        <Stack display={isShown ? "flex" : "none"}>
+          {mapArr?mapArr.map((item, index) => {
+            return (
+              <Stack
+                direction={"row"}
+                p="8px 12px"
+                borderRadius={2}
+                key={index}
+                onClick={() => {
+                  setState(item);
+                  setIsShown((prev) => !prev);
+                }}
+              >
+                <Typography fontWeight={600} color={"#3F4145"}>
+                  {item}
+                </Typography>
+              </Stack>
+            );
+          }):null}
+        </Stack>
+      ) : isShown ? (
+        <Input
+          type="date"
+          onChange={(event) => {
+            [setState(event?.target.value)];
+          }}
+        />
+      ) : null}
+    </Stack>
+  );
+};
