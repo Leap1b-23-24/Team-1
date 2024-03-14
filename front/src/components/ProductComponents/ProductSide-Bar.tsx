28f9a94d81e69c6c4e84c3708f521d5c6a05c3c5
Батмандал Бямба <23LP5716@G301-CC0203-3.local>
2024-03-14 12:09:20 +0800

    update

--- front/src/components/ProductComponents/ProductSide-Bar.tsx
+++ front/src/components/ProductComponents/ProductSide-Bar.tsx
@@ -1,25 +1,38 @@
 import { ContentPaste, Create, Sell, Settings } from "@mui/icons-material";
 import { Stack, Typography } from "@mui/material";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const menu = [
-  { name: "Хяналтын самбар", icon: <Create /> },
-  { name: "Захилга", icon: <ContentPaste /> },
-  { name: "Орлого", icon: <Sell /> },
-  { name: "Бүтээгдэхүүн", icon: <Create /> },
-  { name: "Тохиргоо", icon: <Settings /> },
+  { name: "Хяналтын самбар", icon: <Create />, link: "/dashboard" },
+  { name: "Захилга", icon: <ContentPaste />, link: "orderPage" },
+  { name: "Орлого", icon: <Sell />, link: "incomePage" },
+  { name: "Бүтээгдэхүүн", icon: <Create />, link: "productPage" },
+  { name: "Тохиргоо", icon: <Settings />, link: "/settingsPage" },
 ];
 
 export const ProductSideBar = () => {
+  const pathName = usePathname();
   return (
     <Stack width="12%" py={3} height="100vh" gap={2}>
       {menu.map((item, index) => {
         return (
-          <Stack key={index} direction="row" gap={1} py={1} marginLeft={2}>
-            {item.icon}
-            <Typography fontSize="16px" fontWeight={600}>
-              {item.name}
-            </Typography>
-          </Stack>
+          <Link href={item.link} style={{ textDecoration: "none" }}>
+            <Stack
+              key={index}
+              direction="row"
+              gap={1}
+              py={1}
+              paddingLeft={2}
+              color="#121316"
+              bgcolor={pathName == item.link ? "red" : ""}
+            >
+              {item.icon}
+              <Typography fontSize="16px" fontWeight={600}>
+                {item.name}
+              </Typography>
+            </Stack>
+          </Link>
         );
       })}
     </Stack>
