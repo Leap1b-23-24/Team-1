a997e19bf643ed0d4c9e1f099af4322b477289ad
Tsolmon Togtokhsaikhan <23LP9801@G301-CC0188.local>
2024-03-19 09:22:24 +0800

    init

--- api/src/routers/auth.router.ts
+++ api/src/routers/auth.router.ts
@@ -3,6 +3,6 @@ import { signUp } from "../controllers/auth.controller";
 
 const authRouter = Router();
 
-authRouter.post("signUp", signUp);
+authRouter.post("/signUp", signUp);
 
 export default authRouter;
