1b72f581858c7df75f2de1b0e46f11a7595393aa
Tsolmon Togtokhsaikhan <23LP9801@G301-CC0188.local>
2024-03-22 12:15:00 +0800

    init

--- front/src/providers/OrderProvider.tsx
+++ front/src/providers/OrderProvider.tsx
@@ -1,4 +1,12 @@
-import { Children, createContext, useContext, useState } from "react";
+import { api } from "@/common";
+import {
+  Children,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { toast } from "react-toastify";
 
 type OrderProviderType = {
   children: React.ReactNode;
@@ -17,6 +25,24 @@ const OrderContext = createContext<OrderContextType>({} as OrderContextType);
 
 export const OrderProvider = ({ children }: OrderProviderType) => {
   const [order, setOrder] = useState<Order[]>([]);
+
+  useEffect(() => {
+    getOrder;
+  });
+
+  const getOrder = async () => {
+    try {
+      const res = await api.get("/order/get", {
+        headers: { Authorization: localStorage.getItem("token") },
+      });
+
+      const { order } = res.data;
+
+      setOrder(order);
+    } catch (error: any) {
+      toast.error(error.response.data.message);
+    }
+  };
   return (
     <OrderContext.Provider value={{ order }}>{children}</OrderContext.Provider>
   );
